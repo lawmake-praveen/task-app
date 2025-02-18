da507245eb3971cd90182aa01385351dbdec3c5d
Lawmake Praveen <152585994+lawmakepraveen14@users.noreply.github.com>
2025-02-18 17:27:19 +0530

    build

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -26,7 +26,7 @@ const Chat = () => {
       <div className="top">
         <h1>Task App</h1>
         {/* <p onClick={() => navigate("/chat")}>Chat</p> */}
-        <p onClick={logout}>Logout</p>
+        <p onClick={logout}>Log out</p>
       </div>
       <InputArea
         update={update}
